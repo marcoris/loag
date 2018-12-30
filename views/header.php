a98e442defabf5e79f4b51585b5f134f381472e9
Marco Ris <maro.ris@risdesign.ch>
2018-12-30 18:59:57 +0100

    Add open sans font

--- views/header.php
+++ views/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo URL; ?>public/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">    <link rel="stylesheet" href="<?php echo URL; ?>public/css/style.css">
     <title>PHP-MVC</title>
 </head>
 <body>
