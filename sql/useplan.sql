34240e3202d740527e156d2fff4026bd6c28cf32
Marco Ris <ris@contria.com>
2019-03-14 09:51:32 +0100

    ° Change table useplan + Add table train

--- sql/useplan.sql
+++ sql/useplan.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `useplan` (
   `useplan_id` int(11) NOT NULL AUTO_INCREMENT,
   `useplan_line_id` int(11) DEFAULT NULL,
+  `useplan_train_id` int(11) DEFAULT NULL,
   `useplan_date` varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  `useplan_train_nr` varchar(45) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`useplan_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
